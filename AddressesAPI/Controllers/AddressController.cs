085eb9b006cc9142840e2ffd835f6d7a9c7198c7
goodkod <grom13-13@yandex.ru>
2021-06-08 00:42:21 +0700

    Добавьте файлы проекта.

---
+++ AddressesAPI/Controllers/AddressController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using AddressesAPI.Models;
+
+namespace AddressesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AddressController : ControllerBase
+    {
+        private Address address = new Address();
+        private readonly ILogger<AddressController> _logger;
+        public AddressController(ILogger<AddressController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{searchString}")]
+        public List<string> SearchAddresses(string searchString)
+        {
+            List<string> addresses = new List<string>();
+            addresses = address.Search(searchString);
+            LogSearch(addresses.Count());
+            return addresses;
+        }
+
+        [HttpPost("{newAddress}")]
+        public void CreateNewAddress(string newAddress)
+        {
+            
+            if (address.VerificationNewAddress(newAddress) == true)
+            {
+                address.AddAddress(newAddress);
+            }
+            LogAddAddresses(address.VerificationNewAddress(newAddress), newAddress);
+        }
+
+        private void LogAddAddresses(bool verificationAddress, string newAddress)
+        {
+            if (verificationAddress)
+            {
+                _logger.LogInformation("Add address" + newAddress);
+            }
+            else
+            {
+                _logger.LogError("The string does not match the criterion");
+            }
+        }
+
+        private void LogSearch(int quantityAddresses)
+        {
+            if (quantityAddresses != 0)
+            {
+                _logger.LogInformation("Found " + quantityAddresses + "addresses");
+            }
+            else
+            {
+                _logger.LogInformation("Not found");
+            }
+        }
+    }
+}
---
+++ AddressesAPI/Models/Address.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+
+namespace AddressesAPI.Models
+{
+    public class Address
+    {
+        
+        private string pathFile = "addresses.txt";
+        public bool VerificationNewAddress(string newAddress)
+        {
+            string stringVerification = @"\d{6},\s{1}\w+\s{1}\w+,\s{1}\w+\s{1}\w+,\s{1}город\s{1}\w+,\s{1}\d+";
+            if (Regex.IsMatch(newAddress, stringVerification, RegexOptions.IgnoreCase))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void AddAddress(string newAddress)
+        {
+            newAddress = "\n" + newAddress;
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);//Add all text encodings 
+            System.IO.File.AppendAllText(pathFile, newAddress, Encoding.GetEncoding(1251));
+        }
+
+        public List<string> Search(string searchString)
+        {
+            List<string> addresses = new List<string>();
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);//Add all text encodings 
+            using (FileStream fileWithAddresses = System.IO.File.Open(pathFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (BufferedStream buffer = new BufferedStream(fileWithAddresses))
+            using (StreamReader fileLine = new StreamReader(buffer, Encoding.GetEncoding(1251)))
+            {
+                string line;
+                while ((line = fileLine.ReadLine()) != null)
+                {
+                    if (line.Contains(searchString))
+                    {
+                        addresses.Add(line);
+                    }
+                }
+            }
+            return addresses;
+        }
+        
+    }
+}
